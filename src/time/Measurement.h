a0207ed18baafdcdd8d36ab7041f5921ae1780a1
Dominik Lau <harek2388@gmail.com>
2023-10-23 13:25:27 +0200

    experiment scenario

--- src/time/Measurement.h
+++ src/time/Measurement.h
@@ -12,19 +12,22 @@
 namespace sbd::time {
 	class Measurement {
 	public:
-		Measurement(std::ostream& in, MeasurementAggr& aggr, Operation op) :
+		Measurement(std::ostream& in, MeasurementAggr& aggr, Operation op, bool verbose = true) :
             in(in),
             startTimestamp({ writeClock().get(), readClock().get()}),
             aggr(aggr),
-            op(op){};
+            op(op), verbose(verbose) {};
 		~Measurement() {
 			auto writes = writeClock().get() - startTimestamp.write;
 			auto reads = readClock().get() - startTimestamp.read;
-			in << "[Measurement] r: " << reads << " w: " << writes << " io(r+w): " << writes + reads << std::endl;
+            if(verbose){
+                in << "[Measurement] r: " << reads << " w: " << writes << " io(r+w): " << writes + reads << std::endl;
+            }
             aggr.addRead(op, reads);
             aggr.addWrite(op, writes);
 		}
 	private:
+        bool verbose = true;
 		std::ostream& in;
 		struct Timestamp {
 			uint64_t write, read;
--- test/perf2.cpp
+++ test/perf2.cpp
@@ -14,28 +14,54 @@ public:
         // we want deterministic generation to be able to reproduce the experiment's outcome
         srand(0);
     }
-    void generateInserts(size_t n){
-        for(auto i=0u; i < n; i++){
-            time::Measurement meas(std::cout, aggr, INSERT);
-            indexedFile.insert(rand()%constants::MAX_RECORD_KEY, generateCarNumber());
-        }
-    }
 
 protected:
-    impl::IndexedFile indexedFile;
     time::MeasurementAggr aggr;
 };
 
+void generateInserts(impl::IndexedFile& indexedFile, time::MeasurementAggr& aggr, size_t n){
+    for(auto i=0u; i < n; i++){
+        time::Measurement meas(std::cout, aggr, INSERT, false);
+        indexedFile.insert(rand()%constants::MAX_RECORD_KEY, generateCarNumber());
+    }
+}
 
-TEST_F(Experiment, sizesTest){
+
+TEST_F(Experiment, sizesAlphaTest){
     std::cout << "[INFO] alpha=" << constants::REORGANISATION_ALPHA << std::endl;
-    generateInserts(1000);
-    indexedFile.blockPrint(std::cout);
-    indexedFile.inorderPrint(std::cout);
-    aggr.printReport();
+    impl::IndexedFile indexedFile;
+    generateInserts(indexedFile, aggr, 1000);
     indexedFile.reportSize(std::cout);
 }
 
+TEST_F(Experiment, sizesNTest){
+    static constexpr auto Ns = {50, 250, 500, 1000, 2500, 5000, 7500, 10000};
+    for(auto N : Ns){
+        std::cout << "meas for " << N << std::endl;
+        impl::IndexedFile indexedFile_;
+        generateInserts(indexedFile_, aggr, N);
+        indexedFile_.reportSize(std::cout);
+    }
+}
+
 TEST_F(Experiment, insertPerfTest){
+    impl::IndexedFile indexedFile;
+    generateInserts(indexedFile, aggr, 1000);
+    aggr.printReport();
+}
+
+TEST_F(Experiment, updateExistingTest){
+
+}
+
+TEST_F(Experiment, updateNonExistingTest){
+
+}
+
+TEST_F(Experiment, removeExistingTest){
+
+}
+
+TEST_F(Experiment, removeNonExistingTest){
 
 }
